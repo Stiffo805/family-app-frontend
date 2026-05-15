2ce3db12cf3616e1890b74824ff2ff7adb5eb477
Adam Konior <akonior632@gmail.com>
2026-05-15 20:14:36 +0200

    Actually filtering on frontend

--- src/api/itemRegister.tsx
+++ src/api/itemRegister.tsx
@@ -1,9 +1,9 @@
 import useCustomQuery from '@src/api/hooks/useCustomQuery'
 import type { ItemsRegister } from '@src/util/types'
 
-export const useGetItemsRegister = (args: { searchText: string }) =>
+export const useGetItemsRegister = () =>
   useCustomQuery<ItemsRegister>({
     method: 'GET',
-    url: `/item_register/register/?searchText=${args.searchText}`,
-    queryKey: ['itemsRegister', args.searchText]
+    url: `/item_register/register/`,
+    queryKey: ['itemsRegister']
   })
